c2938b75f513482de69c54b73e1271add4189939
KathidB <Kathidbb@gmail.com>
2023-10-11 10:40:54 +0200

    working on useReducer for table reservation

--- src/components/Pages/BookingPage.js
+++ src/components/Pages/BookingPage.js
@@ -1,37 +1,14 @@
 import BookingForm from '../data/BookingForm'
-import { useReducer, useState } from 'react'
+import { useState } from 'react'
 
 function BookingPage () {
-  // const [availableTimes, setAvailableTimes] = useState([
-  //   '17:00',
-  //   '18:00',
-  //   '19:00',
-  //   '20:00',
-  //   '21:00'
-  // ])
-
-  const [availableTimes, setAvailableTimes] = useReducer(
-    updateTimes,
-    initializeTimes
-  )
-
-  function updateTimes (state, action) {
-    if (action.type === '17:00') return { time: '17:00' }
-    if (action.type === '18:00') return { time: '18:00' }
-    return state
-  }
-
-  function initializeTimes () {
-    return [
-      '17:00',
-      '18:00',
-      '19:00',
-      '20:00',
-      '21:00'
-    ];
-  }
-
-  console.log(availableTimes)
+  const [availableTimes, setAvailableTimes] = useState([
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00'
+  ])
 
   return (
     <div className='wrapper'>
