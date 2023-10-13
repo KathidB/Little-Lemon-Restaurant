94642c3d60073067677e9f397810d872e09baff5
KathidB <Kathidbb@gmail.com>
2023-10-13 17:26:06 +0200

    finalized useReducer hook to work with the form, it updates hours in the form based on the date that user pick

--- src/components/Pages/BookingPage.js
+++ src/components/Pages/BookingPage.js
@@ -2,28 +2,35 @@ import BookingForm from '../data/BookingForm'
 import { useReducer } from 'react'
 import { fetchAPI } from '../data/bookingsAPI'
 
-function BookingPage () {
-  const [availableTimes, setAvailableTimes] = useReducer(
-    updateTimes,
-    initializeTimes()
-  )
+//DOKUMENTACJA:
 
-  function updateTimes (action) {
-    console.log(action);
-    // console.log(state);
-    const newDate = action.date // Pobierz nową datę z akcji
-    return fetchAPI(newDate) // Użyj wybranej daty do aktualizacji dostępnych czasów
+function BookingPage () {
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes())
+  //Function accepts action - as a result of dispatch from hangleDateChange.
+  //That date is passed to API and API returns a list of hours.
+  //That hours are used to populate availableTimes and that is used to map hours in
+  // the booking form. So hours are different everytime user changes the date of table reservation.
+
+  function updateTimes (state, action) {
+    const newDate = action.date
+    return fetchAPI(newDate)
   }
 
+  // This function accepts the date selected by the user in a form. The selected date is then passed
+  // to the onDateChange(newDate) function used in the BookingPage component. When the user changes the date,
+  // the function dispatches that date to the updateTimes function. This process occurs every time the date is changed.
+  function handleDateChange (newDate) {
+    dispatch({ date: newDate })
+  }
+
+  // This function accepts an action, which is the result of a dispatch from handleDateChange.
+  // The date from the action is then passed to the API, which returns a list of available hours.
+  // These hours are used to populate the availableTimes state, which is then used to map the available hours
+  // in the booking form. As a result, the hours are different every time the user changes the date for table reservation.
+  // APP Written by B.B. if uploaded by someone else then it was stolen. Thanks.
   function initializeTimes () {
     let today = new Date().toISOString().split('T')[0]
     return fetchAPI(today)
-  }
-
-  function handleDateChange (newDate) {
-    console.log(newDate)
-    // Wywołaj funkcję dispatch, aby zaktualizować dostępne czasy w reducerze
-    setAvailableTimes({ date: newDate })
   }
 
   return (
@@ -31,11 +38,11 @@ function BookingPage () {
       <h2>Booking Page</h2>
       <BookingForm
         availableTimes={availableTimes}
-        setAvailableTimes={setAvailableTimes}
+        dispatch={dispatch}
         onDateChange={handleDateChange}
       />
     </div>
   )
 }
-
+// Written by B.B. if uploaded by someone else then it was stolen. Thanks.
 export default BookingPage
