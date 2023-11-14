ebe2b7b0d7b6161b133be8db8eb1c96e43521b2a
KathidB <Kathidbb@gmail.com>
2023-11-14 13:30:01 +0100

    fixed to react routing

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -11,7 +11,7 @@ function Routing () {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='' element={<Main />} />
         <Route path='/About' element={<About />} />
         <Route path='/Menu' element={<Menu />} />
         <Route path='/BookingPage' element={<BookingPage />} />
